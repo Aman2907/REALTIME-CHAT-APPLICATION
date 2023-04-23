6b2c40da08ad7fb7a92471aa006147dff4922dab
Aman Sharma <74008888+Aman2907@users.noreply.github.com>
2023-04-23 11:19:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const io = require('socket.io')(8000, {
 
   const users = {};
  
-
 io.on('connection', socket =>{
     socket.on('new-user-joined',name =>{
         console.log("new user", name);
